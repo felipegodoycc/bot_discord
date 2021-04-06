859ac5a504a6ca81d4d83ec536936d9d39cc17dc
Felipe Godoy Cerda <felipe.godoy@ceinf.cl>
2021-04-06 19:18:45 -0400

    add nuevas funciones y se mueven a este archivo segun uso

--- src/music/utils.ts
+++ src/music/utils.ts
@@ -1,4 +1,5 @@
-import { EmbedField, MessageEmbed } from "discord.js";
+import { EmbedField, Message, MessageEmbed } from "discord.js";
+import ytsr from "ytsr";
 import { Song } from "../types/song";
 
 export function isUrl(data: string): Boolean{
@@ -12,4 +13,30 @@ export function createEmbebedMessage(songs: Song[]): MessageEmbed {
         .setTitle("Lista de canciones en la cola")
         .addFields(songsList);
     return embed;
+}
+
+export function sleep(ms: number): Promise<void>{
+    return new Promise( (resolve) => {
+        setInterval( () => resolve(), ms)
+    })
+}
+
+export function getLink(toSearch: string): Promise<string>{
+    return new Promise( async (resolve) => {
+        const filters = await ytsr.getFilters(toSearch);
+        const filter = filters.get('Type').get('Video');
+        const search: any = await ytsr(filter.url, { limit: 5 });
+        resolve(search.items[0].url)
+    })
+}
+
+export function getParamsFromMessage(message: Message){
+    const msg = message.content;
+    console.log(message.content.split(' '))
+    const [ ,command, ...resto ] = msg.split(' ');
+    console.log('Mensaje: ', command, ' - request: ', resto.join(' '))
+    return {
+        command,
+        request: resto.join(' ')
+    }
 }

6d3ee2833abf552ca11aced615ba48a5f1f53a0a
Felipe Godoy Cerda <felipe.godoy@ceinf.cl>
2021-04-09 14:27:26 -0400

    add mensajes de respuesta

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ export const PREFIX = '!'
 export const LISTENCHANNEL = 'test-bot'
 export const GENERALCHANNEL = 'general'
 export const PLAYLIST_LIMIT = 15;
+export const DEDICATED_MUSIC_TEXT_CHANNEL = 'music-bot-dedicated'
 export const MESSAGES = {
     INVALID_COMMAND: 'Debes ingresar un comando valido :( !',
     MOVE_SUCCESS: 'Listo, movido',
@@ -21,5 +22,8 @@ export const MESSAGES = {
     NOT_QUEUE_EXIST: 'Quien esta fumando webon? no hay lista webon',
     WELCOME_VOICE_CHAT: '{0} se ha conectado a {1}, ta solito, quiere pito.',
     OUT_OF_QUEUE_RANGE: 'Wou wou wou esta cancion no esta en la lista',
-    NOT_ALLOWED_ORIGIN: 'Link mas enviado u origen no soportado'
+    NOT_ALLOWED_ORIGIN: 'Link mas enviado u origen no soportado',
+    WELCOME_DEDICATED_MUSIC_CHANNEL: 'Bienvenido al canal dedicado de musica, solo debes enviar la cancion o link, sin ningun comando',
+    DEDICATED_CHANNEL_EXIST: 'El canal dedicado ya existe',
+    DEDICATE_CHANNEL_SUCCESFULL: 'listo, busca el canal de texto {0} y reproduce musica directamente sin comandos'
 }
